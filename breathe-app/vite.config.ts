4705fc10e764bb3965899ff454344a0c2c707065
nachlielEasy <33023571+nachpok@users.noreply.github.com>
2024-03-05 19:03:09 +0200

    VitePWA and VitePWAOptions

--- breathe-app/vite.config.ts
+++ breathe-app/vite.config.ts
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: [
+    "favicon.ico",
+    "robots.txt",
+    "B-144x144.png",
+    "B-512x512.png",
+  ],
+  manifest: {
+    name: "Breathe",
+    short_name: "B",
+    theme_color: "#000000",
+    background_color: "#FFFFFF",
+    display: "standalone",
+    start_url: "/index.html",
+    scope: "/",
+    icons: [
+      {
+        src: "public/B.ico",
+        sizes: "16x16",
+        type: "image/ico",
+      },
+      {
+        src: "public/B-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "public/B-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "public/B-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+};
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
