2d6a8a1d966bd1a0a7b97ad047402dd26de4c7d7
nachlielEasy <33023571+nachpok@users.noreply.github.com>
2024-03-06 13:52:24 +0200

    Update service-worker.js

--- breathe-app/service-worker.js
+++ breathe-app/service-worker.js
@@ -1,23 +1,23 @@
-// service-worker.js
-// self.addEventListener("install", function (e) {
-//   e.waitUntil(
-//     caches.open("breathe").then(function (cache) {
-//       return cache.addAll([
-//         "/",
-//         "/index.html",
-//         "/src/Components/Timer.css",
-//         "/scripts/app.js",
-//         // Add other resources you want cached
-//       ]);
-//     })
-//   );
-// });
+// service - worker.js;
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open("breathe").then(function (cache) {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/src/Components/Timer.css",
+        "/scripts/app.js",
+        // Add other resources you want cached
+      ]);
+    })
+  );
+});
 
-// self.addEventListener("fetch", function (event) {
-//   console.log(event.request.url);
-//   event.respondWith(
-//     caches.match(event.request).then(function (response) {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
+self.addEventListener("fetch", function (event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
